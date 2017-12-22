1daa68338695066a5b336abeed7ccfcd4dcb3c36
Rerikn <Happyskalolaz@outlook.com>
2017-12-22 15:39:35 +0500

    Initial  commit
    Stroke style

--- app/src/main/java/com/example/ballsphysics/BallSource/Ball.java
+++ app/src/main/java/com/example/ballsphysics/BallSource/Ball.java
@@ -58,7 +58,7 @@ public class Ball extends View {
     public void paint(Canvas canvas) {
         canvas.drawColor(Color.TRANSPARENT);
         Paint ballColor = new Paint();
-        ballcolor.setStyle(Paint.Style.STROKE);
+      //  ballcolor.setStyle(Paint.Style.STROKE);
         ballColor.setARGB(transp, red, green, blue);
         canvas.drawCircle(position.getX(), position.getY(), radius, ballColor);
     }
